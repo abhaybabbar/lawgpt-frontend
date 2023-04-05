05f7d47a21a45fa4eb9197a3e27f35ddccf559b4
Abhay Babbar <babbarabhay1852@gmail.com>
2023-04-05 12:38:03 +0530

    converted backend to https from http

--- pages/api/_healper.js
+++ pages/api/_healper.js
@@ -1,3 +1,3 @@
 // const backend_url = "http://127.0.0.1:8000";
-const backend_url = "http://backend.quensulting.com";
+const backend_url = "https://backend.quensulting.com";
 export { backend_url };
